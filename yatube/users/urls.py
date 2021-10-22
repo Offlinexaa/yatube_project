464fc018a762aa1cd56b5bdfcf4ca245df106188
Offlinexaa <Offlinexaa@gmail.com>
2021-10-22 19:57:29 +0400

    Добавлен кастомный фильтр, исправлен шаблон signup

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
+from . import views
 
 app_name = 'users'
 
@@ -11,5 +12,10 @@ urlpatterns = [
         'logout/',
         LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
+    ),
+    path(
+        'signup/',
+        views.SignUp.as_view(),
+        name='signup'
     ),
 ]
